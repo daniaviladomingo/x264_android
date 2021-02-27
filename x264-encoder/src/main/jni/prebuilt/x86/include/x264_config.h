a0f9cba3135d6611b927498dcb352f1340ea9e95
UltraBook <daniaviladomingo@gmail.com>
2021-02-27 14:19:02 +0100

    build script x264

--- x264-encoder/src/main/jni/prebuilt/x86/include/x264_config.h
+++ x264-encoder/src/main/jni/prebuilt/x86/include/x264_config.h
@@ -2,7 +2,7 @@
 #define X264_INTERLACED    1
 #define X264_BIT_DEPTH     0
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 3043
+#define X264_REV 3048
 #define X264_REV_DIFF 0
-#define X264_VERSION " r3043 59c0609"
-#define X264_POINTVER "0.161.3043 59c0609"
+#define X264_VERSION " r3048 b86ae3c"
+#define X264_POINTVER "0.161.3048 b86ae3c"
